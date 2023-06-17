3d6bda215e4617afabbe57a8f730f791d2cf29d6
Timur Dian Radha Sejati <timur7dian84@gmail.com>
2023-06-17 23:05:01 +0700

    Initial commit

---
+++ components/index.ts
@@ -0,0 +1,22 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  SearchBar,
+  CustomFilter,
+  CarCard,
+  CarDetails,
+  ShowMore,
+  Footer,
+};
